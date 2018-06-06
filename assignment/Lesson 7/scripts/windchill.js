6330773e0da4bdd5a832116b75761cb12d37c124
smankferdinand <31794585+smankferdinand@users.noreply.github.com>
2018-06-06 03:33:46 +0000

    lesson-7 CSS Flex box and grid

---
+++ assignment/Lesson 7/scripts/windchill.js
@@ -0,0 +1,11 @@
+function computeNum(){
+				var tempo = parseFloat(document.getElementById("temperature").value);
+				var speed = parseFloat(document.getElementById("windSpeed").value);
+				 
+				document.getElementById("outman").innerHTML = windChill(tempo, speed).toPrecision(3);
+			}
+				function windChill(tempo, speed){
+					return 35.74 + .6215 * tempo - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempo * Math.pow(speed, 0.16);
+				}
+			
+computeNum();
